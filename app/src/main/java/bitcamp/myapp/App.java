7b52e1c73dc82ece8343bd58a21168eb526296cc
perniteo <perniteo1@gmail.com>
2024-04-05 12:40:20 +0900

    spring boot app

--- app/src/main/java/bitcamp/myapp/App.java
+++ app/src/main/java/bitcamp/myapp/App.java
@@ -10,14 +10,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @SpringBootApplication
 @EnableTransactionManagement
 @PropertySource({
-    "classpath:config/ncp.properties",
-    "classpath:config/ncp-secret.properties"
+    "file:${user.home}/config/jdbc.properties",
+    "file:${user.home}/config/ncp.properties",
+    "file:${user.home}/config/ncp-secret.properties"
 })
 @Controller
 public class App {
 
   public static void main(String[] args) throws Exception {
     System.out.println("과제관리 시스템 서버 실행!");
+    String userHome = System.getProperty("user.home");
+    System.out.println("User Home Directory: " + userHome);
     SpringApplication.run(App.class, args);
   }
 
